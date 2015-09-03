7bcc5d9d98f059adce9b2de8e15154c5f1be2021
Marco Sgrignuoli <marco.sgrignuoli@gmail.com>
2015-09-03 17:21:31 +0200

    Initial Commit

---
+++ tippp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippp
+//
+//  Created by Marco Sgrignuoli on 9/3/15.
+//  Copyright (c) 2015 Optimizely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
